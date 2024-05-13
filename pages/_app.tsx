cba45ff798f08ce10d62f79b2780479d59b82a55
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-13 14:58:23 +0900

    폰트 설정, global 스타일 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { Noto_Sans_KR } from 'next/font/google';
+import '@/styles/global.css';
+
+const notoSansKR = Noto_Sans_KR({ weight: ['400', '700'], subsets: [] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSansKR.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
