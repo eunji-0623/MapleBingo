3c95bcef3d6697e75ec1f65f110c081b1486d0ed
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-13 16:12:51 +0900

    기본 레이아웃 설정
    1. 페이지 타이틀 및 파비콘 적용
    2. 메인 페이지 기본 레이아웃 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Head>
+        <title>메린이 빙고</title>
+        <link rel="icon" href="/favicon.jpg" />
+      </Head>
+      <div>
+        <header></header>
+        <main></main>
+        <footer></footer>
+      </div>
+    </>
+  );
 }
